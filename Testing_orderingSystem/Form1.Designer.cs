cb2381b2e4ab70ccea994b444418eb5a2df494bd
YokeLing97 <fooyokeling7011@gmail.com>
2018-08-14 15:36:30 +0800

    FTY

---
+++ Testing_orderingSystem/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Testing_orderingSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox_Description = new System.Windows.Forms.TextBox();
+            this.textBox_Price = new System.Windows.Forms.TextBox();
+            this.textBox_Name = new System.Windows.Forms.TextBox();
+            this.textBox_UDescription = new System.Windows.Forms.TextBox();
+            this.textBox_UPrice = new System.Windows.Forms.TextBox();
+            this.textBox_UName = new System.Windows.Forms.TextBox();
+            this.button_Add = new System.Windows.Forms.Button();
+            this.button_Remove = new System.Windows.Forms.Button();
+            this.button_Update = new System.Windows.Forms.Button();
+            this.button_Exit = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox_Search = new System.Windows.Forms.TextBox();
+            this.button_Display = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(290, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Updated Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Price";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(290, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Updated Price";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Description";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(290, 141);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(155, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Updated Description";
+            // 
+            // textBox_Description
+            // 
+            this.textBox_Description.Location = new System.Drawing.Point(127, 135);
+            this.textBox_Description.Multiline = true;
+            this.textBox_Description.Name = "textBox_Description";
+            this.textBox_Description.Size = new System.Drawing.Size(157, 26);
+            this.textBox_Description.TabIndex = 6;
+            // 
+            // textBox_Price
+            // 
+            this.textBox_Price.Location = new System.Drawing.Point(127, 73);
+            this.textBox_Price.Name = "textBox_Price";
+            this.textBox_Price.Size = new System.Drawing.Size(157, 26);
+            this.textBox_Price.TabIndex = 7;
+            // 
+            // textBox_Name
+            // 
+            this.textBox_Name.Location = new System.Drawing.Point(127, 16);
+            this.textBox_Name.Name = "textBox_Name";
+            this.textBox_Name.Size = new System.Drawing.Size(157, 26);
+            this.textBox_Name.TabIndex = 8;
+            // 
+            // textBox_UDescription
+            // 
+            this.textBox_UDescription.Location = new System.Drawing.Point(451, 135);
+            this.textBox_UDescription.Name = "textBox_UDescription";
+            this.textBox_UDescription.Size = new System.Drawing.Size(157, 26);
+            this.textBox_UDescription.TabIndex = 9;
+            // 
+            // textBox_UPrice
+            // 
+            this.textBox_UPrice.Location = new System.Drawing.Point(451, 76);
+            this.textBox_UPrice.Name = "textBox_UPrice";
+            this.textBox_UPrice.Size = new System.Drawing.Size(157, 26);
+            this.textBox_UPrice.TabIndex = 10;
+            // 
+            // textBox_UName
+            // 
+            this.textBox_UName.Location = new System.Drawing.Point(451, 19);
+            this.textBox_UName.Name = "textBox_UName";
+            this.textBox_UName.Size = new System.Drawing.Size(157, 26);
+            this.textBox_UName.TabIndex = 11;
+            // 
+            // button_Add
+            // 
+            this.button_Add.Location = new System.Drawing.Point(127, 200);
+            this.button_Add.Name = "button_Add";
+            this.button_Add.Size = new System.Drawing.Size(126, 83);
+            this.button_Add.TabIndex = 12;
+            this.button_Add.Text = "ADD";
+            this.button_Add.UseVisualStyleBackColor = true;
+            this.button_Add.Click += new System.EventHandler(this.button_Add_Click);
+            // 
+            // button_Remove
+            // 
+            this.button_Remove.Location = new System.Drawing.Point(385, 200);
+            this.button_Remove.Name = "button_Remove";
+            this.button_Remove.Size = new System.Drawing.Size(126, 83);
+            this.button_Remove.TabIndex = 13;
+            this.button_Remove.Text = "REMOVE";
+            this.button_Remove.UseVisualStyleBackColor = true;
+            this.button_Remove.Click += new System.EventHandler(this.button_Remove_Click);
+            // 
+            // button_Update
+            // 
+            this.button_Update.Location = new System.Drawing.Point(127, 330);
+            this.button_Update.Name = "button_Update";
+            this.button_Update.Size = new System.Drawing.Size(126, 83);
+            this.button_Update.TabIndex = 14;
+            this.button_Update.Text = "UPDATE";
+            this.button_Update.UseVisualStyleBackColor = true;
+            this.button_Update.Click += new System.EventHandler(this.button_Update_Click);
+            // 
+            // button_Exit
+            // 
+            this.button_Exit.Location = new System.Drawing.Point(257, 431);
+            this.button_Exit.Name = "button_Exit";
+            this.button_Exit.Size = new System.Drawing.Size(126, 83);
+            this.button_Exit.TabIndex = 15;
+            this.button_Exit.Text = "EXIT";
+            this.button_Exit.UseVisualStyleBackColor = true;
+            this.button_Exit.Click += new System.EventHandler(this.button_Close_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeight = 50;
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.dataGridView1.Location = new System.Drawing.Point(664, 58);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(750, 456);
+            this.dataGridView1.TabIndex = 16;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // textBox_Search
+            // 
+            this.textBox_Search.Location = new System.Drawing.Point(792, 13);
+            this.textBox_Search.Name = "textBox_Search";
+            this.textBox_Search.Size = new System.Drawing.Size(561, 26);
+            this.textBox_Search.TabIndex = 18;
+            this.textBox_Search.TextChanged += new System.EventHandler(this.textBox_Search_TextChanged);
+            // 
+            // button_Display
+            // 
+            this.button_Display.Location = new System.Drawing.Point(385, 330);
+            this.button_Display.Name = "button_Display";
+            this.button_Display.Size = new System.Drawing.Size(126, 83);
+            this.button_Display.TabIndex = 19;
+            this.button_Display.Text = "DISPLAY";
+            this.button_Display.UseVisualStyleBackColor = true;
+            this.button_Display.Click += new System.EventHandler(this.button_Display_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(701, 19);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 20);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "SEARCH :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1445, 536);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button_Display);
+            this.Controls.Add(this.textBox_Search);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button_Exit);
+            this.Controls.Add(this.button_Update);
+            this.Controls.Add(this.button_Remove);
+            this.Controls.Add(this.button_Add);
+            this.Controls.Add(this.textBox_UName);
+            this.Controls.Add(this.textBox_UPrice);
+            this.Controls.Add(this.textBox_UDescription);
+            this.Controls.Add(this.textBox_Name);
+            this.Controls.Add(this.textBox_Price);
+            this.Controls.Add(this.textBox_Description);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Pizza Menu";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox_Description;
+        private System.Windows.Forms.TextBox textBox_Price;
+        private System.Windows.Forms.TextBox textBox_Name;
+        private System.Windows.Forms.TextBox textBox_UDescription;
+        private System.Windows.Forms.TextBox textBox_UPrice;
+        private System.Windows.Forms.TextBox textBox_UName;
+        private System.Windows.Forms.Button button_Add;
+        private System.Windows.Forms.Button button_Remove;
+        private System.Windows.Forms.Button button_Update;
+        private System.Windows.Forms.Button button_Exit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox_Search;
+        private System.Windows.Forms.Button button_Display;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Testing_orderingSystem/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Testing_orderingSystem
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ADMIN\Documents\testint_menu.mdf;Integrated Security=True;Connect Timeout=30");
+
+        public object Messagebox { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           display_data();
+        }
+
+        public void display_data()
+        {
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "select * from Menu";
+            command.ExecuteNonQuery();
+            DataTable table = new DataTable();
+            SqlDataAdapter data = new SqlDataAdapter(command);
+            data.Fill(table);
+            dataGridView1.DataSource = table;
+            connection.Close();
+        }
+
+        void ClearAllText(Control con)
+        {
+            foreach (Control c in con.Controls)
+            {
+                if (c is TextBox)
+                    ((TextBox)c).Clear();
+                else
+                    ClearAllText(c);
+            }
+        }
+
+        private void button_Add_Click(object sender, EventArgs e)
+        {
+            if((textBox_Name.Text.Trim() == string.Empty) || (textBox_Price.Text.Trim() == string.Empty) || (textBox_Description.Text.Trim() == string.Empty))
+            {
+                MessageBox.Show("Please Provide Details!!");
+            }
+            else
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.Text;
+                command.CommandText = "Insert into Menu values('" + textBox_Name.Text + "','" + textBox_Price.Text + "','" + textBox_Description.Text + "')";
+                command.ExecuteNonQuery();
+                connection.Close();
+                display_data();
+                MessageBox.Show("Record Inserted SUCCESSFULLY!");
+                ClearAllText(this);
+            }
+        }
+
+        private void button_Remove_Click(object sender, EventArgs e)
+        {
+            if(textBox_Name.Text != "")
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.Text;
+                command.CommandText = "delete from Menu where name = '" + textBox_Name.Text + "'";
+                command.ExecuteNonQuery();
+                connection.Close();
+                display_data();
+                MessageBox.Show("Record Deleted SUCCESSFULLY!");
+                ClearAllText(this);
+            }
+            else
+            {
+                MessageBox.Show("Please Select Record to Delete");
+            }
+        }
+
+        private void button_Update_Click(object sender, EventArgs e)
+        {
+            if (textBox_UName.Text != "" && textBox_UPrice.Text != "" && textBox_UDescription.Text != "")
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.Text;
+                command.CommandText = "update Menu set name='" + textBox_UName.Text + "', price='" + textBox_UPrice.Text + "', description='" + textBox_UDescription.Text + "' where name='" + textBox_Name.Text + "'";//, price= '" +textBox_Price.Text+ "' description= '" +textBox_Description.Text+ "'";
+                command.ExecuteNonQuery();
+                connection.Close();
+                display_data();
+                MessageBox.Show("Record Updated SUCCESSFULLY!");
+                ClearAllText(this);
+            }
+
+            else
+            {
+                MessageBox.Show("Please Provide Details Information!");
+            }
+        }
+
+        //private void button_Search_Click(object sender, EventArgs e)
+        //{
+           
+            /*
+            connection.Open();
+            SqlCommand command = connection.CreateCommand();
+            command.CommandType = CommandType.Text;
+            command.CommandText = "SELECT * from Menu where name= '" + textBox_Search.Text + "'";
+            command.ExecuteNonQuery();
+            DataTable table = new DataTable();
+            SqlDataAdapter data = new SqlDataAdapter(command);
+            data.Fill(table);
+            dataGridView1.DataSource = table;
+            connection.Close();*/
+        //}
+
+        private void button_Display_Click(object sender, EventArgs e)
+        {
+            display_data();
+            textBox_Name.Text = "";
+            textBox_UName.Text = "";
+            textBox_Price.Text = "";
+            textBox_UPrice.Text = "";
+            textBox_Description.Text = "";
+            textBox_UDescription.Text = "";
+        }
+
+        private void button_Close_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Do you want to exit?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+                Application.ExitThread();
+            }
+            else
+            { }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >=0)
+            {
+                DataGridViewRow row = this.dataGridView1.Rows[e.RowIndex];
+                textBox_Name.Text = row.Cells["Name"].Value.ToString();
+                textBox_Price.Text = row.Cells["Price"].Value.ToString();
+                textBox_Description.Text = row.Cells["Description"].Value.ToString();
+            }
+        }
+
+        private void textBox_Search_TextChanged(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand();
+            command.Connection = connection;
+            command.CommandText = "SELECT * from Menu where name LIKE '%'+@name+'%'";
+            command.Parameters.AddWithValue("@name", textBox_Search.Text);
+            SqlDataAdapter data = new SqlDataAdapter(command);
+            DataSet set = new DataSet();
+            data.Fill(set);
+            dataGridView1.DataSource = set.Tables[0];
+            connection.Close();
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Do you want to exit?", "Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (dr == DialogResult.Yes)
+            {
+                Application.ExitThread();
+            }
+            else
+            { }
+
+
+
+        }
+    }
+
+}
